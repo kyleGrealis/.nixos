aeb07dbd588e58985a0ba24f2543cc36d4406bc2
Kyle Grealis <kyle@kyleGrealis.com>
2025-09-03 13:24:41 -0500

    fixed url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kyle's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "nixpgks/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }:
