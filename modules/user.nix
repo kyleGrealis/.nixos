92d7dc9230f5736a8e02ad0a7c501d5c29678e22
Kyle Grealis <kyle@kyleGrealis.com>
2025-09-03 18:03:49 -0500

    user settings

---
+++ modules/user.nix
@@ -0,0 +1,31 @@
+# modules/user.nix
+
+{ config, lib, pkgs, ...}: {
+
+  users.users.kyle = {
+    isNormalUser = true;
+    description = "Kyle";
+    extraGroups = ["networkmanager" "wheel" "input"];
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  networking.hostName = "nixos";
+
+}
