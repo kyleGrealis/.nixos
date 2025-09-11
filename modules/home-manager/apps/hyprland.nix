4a9da1587aafb8e0c087a32cc1dc67e0282bfff5
Kyle Grealis <kyle@kyleGrealis.com>
2025-09-11 13:31:19 -0500

    initial hyprland shtuff

--- modules/home-manager/apps/hyprland.nix
+++ modules/home-manager/apps/hyprland.nix
@@ -1,83 +1,39 @@
 # ./modules/home-manager/apps/hyprland.nix
 
-{ config, lib, pkgs, hyprland, ...}: {
+{ config, lib, pkgs, ...}: 
+let
+  pkgs-hyprland = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
+{
 
-  wayland.windowManager.hyprland = {
-    enable = true;
+  imports = [
+    ./wayland.nix
+    ./dbus.nix
+  ];
 
-    # Use the flake package
-    package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    xwayland.enable = true;
+  security = {
+    pam.services.login.enableGnomeKeyring = true;
+  };
 
-    settings = {
+  services.gnome.gnome-keyring.enable = true;
 
-      # Basic keybindings - ESSENTIAL!
-      "$mod" = "SUPER";
-      
-      bind = [
-        # Launch applications
-        "$mod, RETURN, exec, kitty"
-        "$mod, D, exec, wofi --show drun"
-        "$mod, Q, killactive"
-        "$mod, M, exit"
-        "$mod, F, fullscreen"
-        "$mod, V, togglefloating"
-        
-        # Movement
-        "$mod, H, movefocus, l"
-        "$mod, L, movefocus, r"
-        "$mod, K, movefocus, u"
-        "$mod, J, movefocus, d"
-        
-        # Workspaces
-        "$mod, 1, workspace, 1"
-        "$mod, 2, workspace, 2"
-        "$mod, 3, workspace, 3"
-        "$mod, 4, workspace, 4"
-        "$mod, 5, workspace, 5"
-        
-        # Move windows to workspaces
-        "$mod SHIFT, 1, movetoworkspace, 1"
-        "$mod SHIFT, 2, movetoworkspace, 2"
-        "$mod SHIFT, 3, movetoworkspace, 3"
-        "$mod SHIFT, 4, movetoworkspace, 4"
-        "$mod SHIFT, 5, movetoworkspace, 5"
-      ];
-      
-      bindm = [
-        # Mouse bindings
-        "$mod, mouse:272, movewindow"
-        "$mod, mouse:273, resizewindow"
-      ];
-      
-      # Basic appearance
-      decoration = {
-        rounding = 10;
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-        };
-        drop_shadow = true;
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
-      };
-      
-      animations = {
-        enabled = true;
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-        animation = [
-          "windows, 1, 7, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-        ];
-      };
-
+  programs = {
+    hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      xwayland = { enable = true; };
+      portalPackage = pkgs-hyprland.xdg-desktop-portal-hyprland;
     };
   };
-    
+
+  services.xserver = {
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      enableHidpi = true;
+      # theme = "chili";
+      package = pkgs.sddm;
+    };
+  };
+
 }
