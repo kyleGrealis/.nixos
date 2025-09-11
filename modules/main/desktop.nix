981150941e164aa09dccc8ba22c6e9becc6b7fe8
Kyle Grealis <kyle@kyleGrealis.com>
2025-09-11 17:55:28 -0500

    adding options for SDDM

--- modules/main/desktop.nix
+++ modules/main/desktop.nix
@@ -10,6 +10,7 @@
     # Enable the GNOME Desktop Environment
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
+    # displayManager.sddm.enable = true;
 
     # Configure keymap in X11
     xkb = {
